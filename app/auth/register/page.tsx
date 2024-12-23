1486e669af0f4a97ae7bf4d143776d46a725bc27
wujianguo <lsjustin89@gmail.com>
2024-12-23 13:33:58 +0800

    feat: user manage

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
   return (
     <div className="flex justify-center items-center">
       <div className="sm:w-[400px]">
-        <LoginForm />
+        <LoginForm redirect="/" />
       </div>
     </div>
   )
